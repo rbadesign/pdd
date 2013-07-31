250f2a2276afe6a58fa398d8fb94539963f3cd05
Dmitry Protopopov <dmitry.protopopov@gmail.com>
2013-08-01 00:43:11 +0400

    CLI

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+D:\Projects\pdd\platforms\android\gen\com\rbadesign\pdd\R.java \
+ : D:\Projects\pdd\platforms\android\res\drawable\icon.png \
+D:\Projects\pdd\platforms\android\res\drawable-hdpi\icon.png \
+D:\Projects\pdd\platforms\android\res\drawable-hdpi\ic_launcher.png \
+D:\Projects\pdd\platforms\android\res\drawable-ldpi\icon.png \
+D:\Projects\pdd\platforms\android\res\drawable-ldpi\ic_launcher.png \
+D:\Projects\pdd\platforms\android\res\drawable-mdpi\icon.png \
+D:\Projects\pdd\platforms\android\res\drawable-mdpi\ic_launcher.png \
+D:\Projects\pdd\platforms\android\res\drawable-xhdpi\icon.png \
+D:\Projects\pdd\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+D:\Projects\pdd\platforms\android\res\layout\main.xml \
+D:\Projects\pdd\platforms\android\res\values\strings.xml \
+D:\Projects\pdd\platforms\android\res\xml\config.xml \
+D:\Projects\pdd\platforms\android\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/com/rbadesign/pdd/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rbadesign.pdd;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
