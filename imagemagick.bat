ced76ad2fbf084572e198873d042027eef93f1f8
Dmitry Protopopov <dmitry.protopopov@gmail.com>
2013-08-03 20:26:50 +0400

    loadImage

--- imagemagick.bat
+++ imagemagick.bat
@@ -1,3 +1,3 @@
-Y:\ImageMagick-6.8.6-7\convert background.jpg -resize 1024x500! -gravity Center  ( logo.png -resize 480x480  ) -composite 1024x500.jpg
-Y:\ImageMagick-6.8.6-7\convert background.jpg -resize 180x120! -gravity Center  ( logo.png -resize 100x100  ) -composite 180x120.jpg
+%IMAGEMAGICK%\convert background.jpg -resize 1024x500! -gravity Center  ( logo.png -resize 480x480  ) -composite 1024x500.jpg
+%IMAGEMAGICK%\convert background.jpg -resize 180x120! -gravity Center  ( logo.png -resize 100x100  ) -composite 180x120.jpg
 pause
--- www/js/pdd.js
+++ www/js/pdd.js
@@ -1,5 +1,21 @@
-$(document).on("vclick",".exit",function(event) {
-	if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
-    navigator.app.exitApp();
-	return false;
-});
+///////////////////////////////////////////////
+//
+// PDD application
+//
+// Copyright (c) RBA DESIGN INTERNATIONAL LLC
+// http://rbadesign.us
+//
+// Developer: Dmitry Protopopov
+// protopopov@narod.ru
+//
+///////////////////////////////////////////////
+
+(function ($) {
+
+	$(document).on("vclick",".exit",function(event) {
+		if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
+		navigator.app.exitApp();
+		return false;
+	});
+		
+})(jQuery);
