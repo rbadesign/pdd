4587bf03d45ee22a4b4314499b6c63a51fd35203
dprotopopov <protopopov@narod.ru>
2013-08-10 11:13:43 +0400

    build 2

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,3 +1,26 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [
+    {
+        "file": "plugins\\org.apache.cordova.core.device\\www\\device.js",
+        "id": "org.apache.cordova.core.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.network-information\\www\\network.js",
+        "id": "org.apache.cordova.core.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins\\org.apache.cordova.core.network-information\\www\\Connection.js",
+        "id": "org.apache.cordova.core.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    }
+]
 });
--- platforms/android/assets/www/js/pdd.js
+++ platforms/android/assets/www/js/pdd.js
@@ -1,5 +1,38 @@
-$(document).on("vclick",".exit",function(event) {
-	if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
-    navigator.app.exitApp();
-	return false;
-});
+///////////////////////////////////////////////
+//
+// PDD application
+//
+// Copyright (c) RBA DESIGN INTERNATIONAL LLC
+// http://rbadesign.us
+//
+// Developer: Dmitry Protopopov
+// protopopov@narod.ru
+//
+///////////////////////////////////////////////
+
+(function ($) {
+	
+	$(document).on("vclick",".exit",function(event) {
+		if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
+		navigator.app.exitApp();
+		return false;
+	});
+
+	$(document).on("vclick",".buy-pro",function(event) {
+		if (event.preventDefault) { event.preventDefault(); } else { event.returnValue = false; }
+		try {
+			var devicePlatform = window.device.platform || "default";
+			switch(devicePlatform) {
+				case "Android": openExternalURL("https://play.google.com/store/apps/details?id=com.rbadesign.pddpro"); break;
+				case "BlackBerry": openExternalURL("http://rbadesign.us/pddpro"); break;
+				case "iPhone": openExternalURL("http://rbadesign.us/pddpro"); break;
+				case "webOS": openExternalURL("http://rbadesign.us/pddpro"); break;
+				case "default": openExternalURL("http://rbadesign.us/pddpro"); break;
+			}
+		} catch (e) {
+			window.open("http://rbadesign.us/pddpro","_blank");
+		}
+		return false;
+	});
+		
+})(jQuery);
